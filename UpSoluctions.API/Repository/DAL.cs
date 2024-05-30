b2baf9295caab6b98ea9fa98812e433b8b9cf5e6
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2024-05-29 22:04:41 -0300

    DAL

---
+++ UpSoluctions.API/Repository/DAL.cs
@@ -0,0 +1,37 @@
+﻿using UpSoluctions.Data;
+
+namespace UpSoluctions.API.Repository
+{
+    public class DAL<T> where T : class
+    {
+        private readonly SystemContext _context;
+
+        public DAL(SystemContext context)
+        {
+            this._context = context;
+        }
+        public IEnumerable<T> Listar()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public void Adicionar(T objeto)
+        {
+            _context.Set<T>().Add(objeto);
+            _context.SaveChanges();
+        }
+        public void Atualizar(T objeto)
+        {
+            _context.Set<T>().Update(objeto);
+            _context.SaveChanges();
+        }
+        public void Deletar(T objeto)
+        {
+            _context.Set<T>().Remove(objeto);
+            _context.SaveChanges();
+        }
+        public T? RecuperarPor(Func<T, bool> condicao)
+        {
+            return _context.Set<T>().FirstOrDefault(condicao);
+        }
+    }
+}
