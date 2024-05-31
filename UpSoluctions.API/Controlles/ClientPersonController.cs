20f2ed87139f7b367c8713bdf19b7af13ba0411c
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2024-05-30 23:04:47 -0300

    ClientPersonController

---
+++ UpSoluctions.API/Controlles/ClientPersonController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UpSoluctions.API.Repository.Interfaces;
+using UpSoluctions.Data.Dtos;
+using UpSoluctions.Data.Entities;
+
+namespace UpSoluctions.API.Controlles
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ClientPersonController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public ClientPersonController(IAuthorRepository authorRepository)
+        {
+            this._authorRepository = authorRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ReadAuthorDto>>> SearchAllAsync()
+        { 
+            ICollection<Author> authors = await _authorRepository.GetAllAsync();
+            return Ok(authors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ReadAuthorDto>> SearchByIdAsync(int id)
+        {
+            Author? author = await _authorRepository.SearchByIdAsync(id);
+
+            if (author == null) return NotFound();
+            
+            ReadAuthorDto authorReturn = new ReadAuthorDto( author.Id, author.Name, author.Biography );
+
+            return authorReturn;
+        }
+
+        [HttpPost]
+        public async Task<ReadAuthorDto> AddAsync(CreateAuthorDto authorDto)
+        {
+            Author author = new Author()
+            {
+                Name = authorDto.Name,
+                Biography = authorDto.Biography
+            };
+
+            await _authorRepository.CreateAsync(author);
+
+            ReadAuthorDto authorReturn = new ReadAuthorDto(author.Id, author.Name, author.Biography);
+
+            return authorReturn;
+        }
+
+        [HttpPut("id")]
+        public async Task<ReadAuthorDto> UpdateByIdAsync(UpdateAuthorDto auth, int id)
+        {
+            Author author = new Author()
+            {
+                Id = id,
+                Name = auth.Name,
+                Biography = auth.Biography
+            };
+
+            await _authorRepository.UpdateAsync(author, id);
+
+            ReadAuthorDto authorReturn = new ReadAuthorDto(author.Id, author.Name, author.Biography);
+
+            return authorReturn;
+        }
+
+        [HttpDelete("id")]
+        public async Task<Author> DropUser(int id)
+        {
+            Author author = await _authorRepository.DeleteAsync(id);
+            return author;
+        }
+    }
+}
